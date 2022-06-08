4eda0811cc40cda66b210eaa2516701434033c64
Carlobortolan <carlobortolan@gmail.com>
2022-06-08 10:05:17 +0200

    Added: Client (controller, resource), Server (rest, service) and Flight classes (class diagram)

---
+++ src/main/java/model/City.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+public class City {
+    private String name;
+    private String country;
+    private String coordinates;
+    private Weather weather;
+    private List<String> poi;
+
+    public City(String name, String country, String coordinates, Weather weather, List<String> poi) {
+        this.name = name;
+        this.country = country;
+        this.coordinates = coordinates;
+        this.weather = weather;
+        this.poi = poi;
+    }
+
+    public City(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+}
---
+++ src/main/java/model/FlightCollection.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FlightCollection {
+
+    private List<Flight> flights;
+
+    public FlightCollection() {
+        this.flights = new LinkedList<Flight>();
+    }
+}
