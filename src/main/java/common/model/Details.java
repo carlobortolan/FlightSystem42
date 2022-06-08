30d0ee5d87232770131736d3957bc5683041b5d0
Carlo Bortolan <carlobortolan@tun.de>
2022-06-08 21:53:15 +0200

    Updated: FlightParser Added: TimeUnits in Details

--- src/main/java/common/model/Details.java
+++ src/main/java/common/model/Details.java
@@ -1,36 +1,100 @@
 package common.model;
 
-import java.time.LocalTime;
+import java.time.Duration;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAmount;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class Details {
-    private String timeOfDeparture;
-    private String eta;
+    private LocalDateTime timeOfDeparture;
+    private LocalDateTime eta;
     private String duration;
-    private String delay;
     private int stops;
     private String airline;
     private String aircraftCode;
     private String terminalFrom;
     private String terminalTo;
 
-    public Details(String timeOfDeparture, String eta, String delay, int stops, String airline, String aircraftCode, String terminalFrom, String terminalTo) {
+    public Details(LocalDateTime timeOfDeparture, String duration, int stops, String airline, String aircraftCode, String terminalFrom, String terminalTo) {
         this.timeOfDeparture = timeOfDeparture;
-        this.eta = eta;
-        this.delay = delay;
+        this.eta = Details.calculateETA(timeOfDeparture, duration);
+        System.out.println("Eta = " + eta);
         this.stops = stops;
         this.airline = airline;
-        this.aircraftCode =aircraftCode;
+        this.aircraftCode = aircraftCode;
         this.terminalFrom = terminalFrom;
         this.terminalTo = terminalTo;
     }
-    public Details(String timeOfDeparture, String eta, String delay, String airline, String aircraftCode, String terminalFrom, String terminalTo) {
-        this.timeOfDeparture = timeOfDeparture;
-        this.eta = eta;
-        this.delay = delay;
-        this.stops = 0;
-        this.airline = airline;
-        this.aircraftCode =aircraftCode;
-        this.terminalFrom = terminalFrom;
-        this.terminalTo = terminalTo;
+
+    public static LocalDateTime calculateETA(LocalDateTime timeOfDeparture, String d) {
+
+        int arrYear = timeOfDeparture.getYear();
+        int arrMonth = timeOfDeparture.getMonthValue();
+        int arrDay = timeOfDeparture.getDayOfMonth();
+        int arrHours = timeOfDeparture.getHour();
+        int arrMinutes = timeOfDeparture.getMinute();
+
+
+        long durationMinutes = 0;
+        long durationHours = 0;
+
+        if (d.contains("M") && d.contains("H")) {
+            durationHours = Integer.parseInt(d.substring(0, d.indexOf("H")));
+            durationMinutes = Integer.parseInt(d.substring(d.indexOf("H") + 1, d.indexOf("M")));
+        } else if (d.contains("H")) {
+            durationHours = Integer.parseInt(d.substring(0, d.indexOf("H")));
+        } else if (d.contains("M")) {
+            durationMinutes = Long.parseLong(d.substring(0, d.indexOf("M") - 1));
+        }
+
+
+        arrMinutes += durationMinutes;
+
+        if (arrMinutes >= 60) {
+            arrHours++;
+            arrMinutes %= 60;
+            if (arrHours >= 24) {
+                arrDay++;
+                arrHours %= 24;
+                if ((arrMonth == 1 || arrMonth == 3 || arrMonth == 5 || arrMonth == 7 || arrMonth == 8 || arrMonth == 10 || arrMonth == 12) && arrDay >= 32) {
+                    arrMonth++;
+                    arrDay %= 32;
+                } else {
+                    if ((arrMonth == 2 || arrMonth == 4 || arrMonth == 6 || arrMonth == 9 || arrMonth == 11) && arrDay >= 31) {
+                        arrMonth++;
+                        arrDay %= 31;
+                    }
+                }
+                if (arrMonth >= 12) {
+                    arrYear++;
+                    arrMonth %= 12;
+                }
+            }
+        }
+
+        arrHours += durationHours;
+
+        if (arrHours >= 24) {
+            arrDay++;
+            arrHours %= 24;
+            if ((arrMonth == 1 || arrMonth == 3 || arrMonth == 5 || arrMonth == 7 || arrMonth == 8 || arrMonth == 10 || arrMonth == 12) && arrDay >= 32) {
+                arrMonth++;
+                arrDay %= 32;
+            } else {
+                if ((arrMonth == 2 || arrMonth == 4 || arrMonth == 6 || arrMonth == 9 || arrMonth == 11) && arrDay >= 31) {
+                    arrMonth++;
+                    arrDay %= 31;
+                }
+            }
+            if (arrMonth >= 12) {
+                arrYear++;
+                arrMonth %= 12;
+            }
+        }
+
+
+        return LocalDateTime.of(arrYear, arrMonth, arrDay, arrHours, arrMinutes);
     }
 }
