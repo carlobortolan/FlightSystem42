9ff254a3ce9e5494da77220c9742ab484ba7253d
Carlo Bortolan <carlobortolan@tun.de>
2022-06-08 13:55:33 +0200

    Test

--- src/main/java/lufthansa/FlightParser.java
+++ src/main/java/lufthansa/FlightParser.java
@@ -1,5 +1,28 @@
 package lufthansa;
 
+import common.model.City;
+import common.model.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //TODO @Carlo
 public class FlightParser {
+    private List<Flight> fetchedFlights;
+    public static ArrayList<Flight> fetchFlights(String input) {
+        ArrayList<Flight> fetchedFlights = new ArrayList<>();
+
+        String[] tmp = input.split(",");
+
+        City from, to;
+        String trackingNumber, airline, plane;
+        int connections;
+
+
+        for(String s : tmp) {
+
+        }
+
+        return fetchedFlights;
+    }
 }
