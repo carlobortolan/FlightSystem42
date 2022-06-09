2802376deaef1f8c0256cf40c9b5a928062cb54e
Carlo Bortolan <carlobortolan@tun.de>
2022-06-10 01:42:38 +0200

    Testing new project setup to push to git

---
+++ src/main/java/com/example/eist22t02zweiundvierziger2022/SearchController.java
@@ -0,0 +1,11 @@
+package com.example.eist22t02zweiundvierziger2022;
+
+import model.FlightCollection;
+
+public class SearchController {
+    private FlightCollection flightCollection;
+
+    public SearchController(FlightCollection flightCollection) {
+        this.flightCollection = flightCollection;
+    }
+}
