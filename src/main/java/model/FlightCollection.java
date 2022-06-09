b354b1879f22cdf13cd469c452acc9f696f949c8
Carlobortolan <carlobortolan@gmail.com>
2022-06-10 01:42:38 +0200

    Testing new project setup to push to git

---
+++ src/main/java/model/FlightCollection.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FlightCollection {
+
+    private List<Flight> flights;
+
+    public FlightCollection() {
+        this.flights = new LinkedList<Flight>();
+    }
+
+    public FlightCollection(LinkedList<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public void addFlight(Flight flight) {
+        this.flights.add(flight);
+    }
+
+    public boolean removeFlight(Flight flight) {
+        return this.flights.remove(flight);
+    }
+}
