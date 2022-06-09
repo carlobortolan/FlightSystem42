cb053c9e87f55a5e7af7a6e9913bea4eabc32739
Carlobortolan <carlobortolan@gmail.com>
2022-06-09 11:25:46 +0200

    Test

--- src/main/java/common/model/FlightCollection.java
+++ src/main/java/common/model/FlightCollection.java
@@ -10,4 +10,16 @@ public class FlightCollection {
     public FlightCollection() {
         this.flights = new LinkedList<Flight>();
     }
+
+    public FlightCollection(LinkedList<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public void addFlight(Flight flight) {
+        this.flights.add(flight);
+    }
+
+    public boolean removeFlight(Flight flight) {
+        return this.flights.remove(flight);
+    }
 }
