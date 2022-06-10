76b4a4b85c8ef48b3e18bf2c63dac7b1069453f0
Carlo Bortolan <carlobortolan@tun.de>
2022-06-10 02:18:03 +0200

    Revert "Fixing Project Setup"
    This reverts commit 452725e03cecaf5e55aa19f192f28c080dc0a39c.

---
+++ src/main/java/client/view/FlightView.java
@@ -0,0 +1,30 @@
+package client.view;
+
+import common.model.FlightCollection;
+
+
+public class FlightView {
+    private static final int DEFAULT_WIDTH = 1200;
+    private static final int DEFAULT_HEIGHT = 700;
+    private static final Dimension2D DEFAULT_SIZE = new Dimension2D(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+    public static Dimension2D getPreferredSize() {
+        return DEFAULT_SIZE;
+    }
+
+    private FlightCollection flightCollection;
+
+    public FlightView(FlightCollection flightCollection) {
+        this.flightCollection = flightCollection;
+        setupFlightView();
+        setupImage();
+    }
+
+    private void setupFlightView() {
+
+    }
+
+    private void setupImage() {
+//        getGraphicsContext2D().drawImage(getImage("background.png"), 0, 0, getWidth(), getHeight());
+    }
+}
