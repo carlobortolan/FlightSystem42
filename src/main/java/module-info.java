c228a069548811d3af6a3bfaa0cb9cfad4f19ad1
Carlobortolan <carlobortolan@gmail.com>
2022-06-10 15:27:54 +0200

    Artemis Template

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,19 @@
 module com.example.eist22t02zweiundvierziger2022 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//    requires javafx.web;
+//
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
 //    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+//    requires com.almasb.fxgl.all;
+    requires spring.web;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+//
     opens com.example.eist22t02zweiundvierziger2022 to javafx.fxml;
-    exports com.example.eist22t02zweiundvierziger2022;
+//    exports com.example.eist22t02zweiundvierziger2022;
 }
