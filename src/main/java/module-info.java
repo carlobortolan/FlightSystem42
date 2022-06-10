1e75a8f8ec8c5b17809e13b323cd349858fb6704
Carlobortolan <carlobortolan@gmail.com>
2022-06-10 17:24:29 +0200

    Test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,15 @@
 module com.example.eist22t02zweiundvierziger2022 {
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//    requires javafx.web;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
 //
-//    requires org.controlsfx.controls;
-//    requires com.dlsc.formsfx;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
 //    requires validatorfx;
-//    requires org.kordamp.ikonli.javafx;
-//    requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
 //    requires eu.hansolo.tilesfx;
-//    requires com.almasb.fxgl.all;
+    requires com.almasb.fxgl.all;
     requires spring.web;
     requires spring.boot.autoconfigure;
     requires spring.boot;
