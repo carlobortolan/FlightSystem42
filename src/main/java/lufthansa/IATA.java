c37b0d7d686bb7063bd2737e60b34ed5d37dd219
Fabian Fritz <f.fritz@tum.de>
2022-06-10 08:25:50 +0200

    New Parser for IATA

--- src/main/java/lufthansa/IATA.java
+++ src/main/java/lufthansa/IATA.java
@@ -8,36 +8,57 @@ import java.util.List;
 
 //TODO @Fabian
 public class IATA {
-
-    private String city;
-    private String airport;
+    private String iataCode;
+    private String airportName;
+    private String cityName;
+    private String country;
+    private String region;
     public static List<IATA> destinations = new LinkedList<>();
 
-    public IATA(String city, String airport) {
-        this.city = city;
-        this.airport = airport;
+
+    public IATA(String iataCode, String airportName, String cityName, String country, String region) {
+        this.iataCode = iataCode;
+        this.airportName = airportName;
+        this.cityName = cityName;
+        this.country = country;
+        this.region = region;
+    }
+    public IATA(){
     }
 
-    public static void finddestinations() throws IOException {
-
+    private void finddestinations() throws IOException {
         Path iataList = Path.of("src/main/java/lufthansa/IATA LIST");
         String content = Files.readString(iataList);
         String[] cities = content.split("\n");
 
         for (int i = 0; i < cities.length; i++) {
-            String[] citydetails = cities[i].split("\\|");
-            String name = citydetails[0].trim();
-            String airportCode = "";
-            if(citydetails.length > 1) {
-                airportCode = citydetails[1].trim();
+            String[] details = cities[i].split("\t");
+            if (details.length < 2) {
+                continue;
             }
-            IATA destination = new IATA(name, airportCode);
-            destinations.add(destination);
+            String iata = details[0].trim().substring(0, 3);
+            String airportName = details[2].trim();
+            String cityName;
+            String regionName = "-";
+            String countryName = "-";
+            String[] location = details[3].split(",");
+            cityName = location[0].trim();
+
+            if (location.length > 2) {
+                regionName = location[1].trim();
+                countryName = location[2].trim();
+            }
+            if (location.length == 2) {
+                countryName = location[1].trim();
+            }
+
+            IATA city = new IATA(iata, airportName, cityName, countryName, regionName);
+            destinations.add(city);
+
         }
     }
 
     public static void main(String[] args) throws IOException {
-        finddestinations();
         System.out.println("Ende");
     }
 
