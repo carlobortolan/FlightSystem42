7475e833bf6d51b5c3e60f44377a381d45a4b404
Carlo Bortolan <carlobortolan@tun.de>
2022-06-11 17:22:13 +0200

    Updated SearchHeader

--- src/main/java/com/example/eist22t02zweiundvierziger2022/FlightSystemController.java
+++ src/main/java/com/example/eist22t02zweiundvierziger2022/FlightSystemController.java
@@ -19,6 +19,8 @@ public class FlightSystemController {
 
     @FXML
     private AnchorPane rootPane;
+    @FXML
+    private Button switchButton;
     private String from;
     private String to;
     private String date;
