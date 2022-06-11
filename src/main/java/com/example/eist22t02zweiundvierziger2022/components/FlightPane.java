3c2265d35a27323746e4ae6569e8c35ea83b2ba4
Carlobortolan <carlobortolan@gmail.com>
2022-06-11 23:07:40 +0200

    Updated FlightPane

--- src/main/java/com/example/eist22t02zweiundvierziger2022/components/FlightPane.java
+++ src/main/java/com/example/eist22t02zweiundvierziger2022/components/FlightPane.java
@@ -10,16 +10,25 @@ import model.FlightObject;
 
 public class FlightPane extends GridPane {
     private Flight flight;
-    public FlightPane(Flight flight, int i) {
+    private GridPane gridPane;
+    public FlightPane(Flight flight) {
         super();
         this.flight = flight;
-//        this.setLayoutX(75);
-//        this.setLayoutY(75*i + 75);
+        this.gridPane = new GridPane();
+
         int j =0;
         for(FlightObject flightObject : this.flight.getFlight()) {
-            this.getChildren().addAll(new FlightObjectPane(flightObject, j++));
-//            getItems().addAll(new FlightObjectPane(flightObject, j++));
+//            this.getChildren().addAll(new FlightObjectPane(flightObject, j));
+            this.add(new FlightObjectPane(flightObject, j), 0, j++);
         }
         System.out.println("ADDED NEW FLIGHT");
+        //
+//        int i = 0;
+//        if(this.flight.getFlight() != null) {
+//            for (FlightObject flightObject : this.flight.getFlight()) {
+//                pane.add(new FlightObjectPane(flightObject, i), 0, i++);
+//            }
+//        }
+
     }
 }
