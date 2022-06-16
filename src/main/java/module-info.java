2ef7b4fe8e937053b4ec57c75ad9df2c084f61d6
Fabian Fritz <f.fritz@tum.de>
2022-06-16 13:14:38 +0200

    Includes Safety Video.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,14 @@ module com.example.eist22t02zweiundvierziger2022 {
     requires javafx.fxml;
     requires javafx.web;
 
-//    requires org.controlsfx.controls;
-//    requires com.dlsc.formsfx;
-////    requires validatorfx;
-//    requires org.kordamp.ikonli.javafx;
-//    requires org.kordamp.bootstrapfx.core;
-//    requires eu.hansolo.tilesfx;
-//    requires com.almasb.fxgl.all;
+    requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+//   requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires javafx.media;
 
     opens com.example.eist22t02zweiundvierziger2022 to javafx.fxml;
     exports com.example.eist22t02zweiundvierziger2022;
