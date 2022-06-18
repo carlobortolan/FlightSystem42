3f383e7de5f6feeae1a3b1bc65b2836f9de89f21
Carlo Bortolan <carlobortolan@tun.de>
2022-06-18 16:00:03 +0200

    Implemented MAP, TRIP OVERVIEW,  ATTRACTIONS AT DESTINATION, HOTELS AT DESTINATION, RESTAURANTS AT DESTINATION

--- src/main/java/com/example/eist22t02zweiundvierziger2022/controllers/MapController.java
+++ src/main/java/com/example/eist22t02zweiundvierziger2022/controllers/MapController.java
@@ -1,21 +1,79 @@
 package com.example.eist22t02zweiundvierziger2022.controllers;
 
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import model.City;
 import model.Coordinates;
 import model.Flight;
+import netscape.javascript.JSObject;
+
+import java.io.File;
 
 public class MapController {
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private Button showStartButton;
+    @FXML
+    private Button showDestinationButton;
+    @FXML
+    private Button showAttractionsButton;
+    @FXML
+    private Button showRestaurantsButton;
+    @FXML
+    private Button showHotelsButton;
+
     //TODO: CONNECT CONTROLLER TO API
     public void initialize(Flight flight) {
+
+        WebView browser = new WebView();
         City from = flight.getFrom();
         City to = flight.getTo();
+        browser.getEngine().getLoadWorker()
+                .stateProperty()
+                .addListener((obs, old, neww) ->
+                {
+                    if (neww == Worker.State.SUCCEEDED) {
+                        // Let JavaScript make calls to adder object,
+                        //so we need to inject an [Adder] object into the JS code
+                        JSObject bridge = (JSObject) browser.getEngine()
+                                .executeScript("window");
+                        bridge.setMember("adder", new Adder());
+                    }
+                });
+        //load the html page
+//        File file = new File("src/main/resources/com/example/eist22t02zweiundvierziger2022/index.html");
+//        browser.getEngine().load(file.toURI().toString());
 
-        if(from.getWeather() != null) {
-            Coordinates fromCoordinate = from.getWeather().getCoordinates();
-        }
-        if(to.getWeather() != null) {
-            Coordinates toCoordinate = to.getWeather().getCoordinates();
-        }
+        browser.getEngine().load("https://www.google.com/maps/dir/" + from.getCityName() +",+" + from.getCountry() + "/" + to.getCityName() + ",+" + to.getCountry());
+        vBox.getChildren().addAll(browser);
+
+        this.showStartButton.setOnAction(e -> {
+            browser.getEngine().load("https://www.google.com/maps/place/" + from.getCityName() + ",+" + from.getCountry());
+            vBox.getChildren().setAll(browser);
+        });
+        this.showDestinationButton.setOnAction(e -> {
+            browser.getEngine().load("https://www.google.com/maps/place/" + to.getCityName() + ",+" + to.getCountry());
+            vBox.getChildren().setAll(browser);
+        });
+        this.showAttractionsButton.setOnAction(e -> {
+            browser.getEngine().load("https://www.google.com/maps/search/" + to.getCityName() + ",+" + to.getCountry() + "+attractions");
+            vBox.getChildren().setAll(browser);
+        });
+        this.showRestaurantsButton.setOnAction(e -> {
+            browser.getEngine().load("https://www.google.com/maps/search/Resaurants+" + to.getCityName() + ",+" + to.getCountry());
+            vBox.getChildren().setAll(browser);
+        });
+        this.showHotelsButton.setOnAction(e -> {
+            browser.getEngine().load("https://www.google.com/maps/search/Hotels+" + to.getCityName() + ",+" + to.getCountry());
+            vBox.getChildren().setAll(browser);
+        });
 
         System.out.println(from.getCityName() + " - " + to.getCityName());
     }
