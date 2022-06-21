adae644301cd0bec2b494d461d17b420464f625c
Carlo Bortolan <carlobortolan@tun.de>
2022-06-21 11:21:10 +0200

    Updated POI Class

--- src/main/java/model/POI.java
+++ src/main/java/model/POI.java
@@ -1,4 +1,20 @@
 package model;
 
 public class POI {
+    private String name;
+    private String address;
+    private String link;
+
+    public POI(String link) {
+        this.link = link;
+        this.name = this.findName();
+        this.address = this.findAddress();
+    }
+
+    private String findAddress() {
+        return null;
+    }
+    private String findName() {
+        return null;
+    }
 }
