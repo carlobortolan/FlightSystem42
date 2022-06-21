17dfd7e63475f06a2123da9af7b076e4f4a3a1b5
Carlo Bortolan <carlobortolan@tun.de>
2022-06-21 15:12:50 +0200

    Updated Controllers to accept POI List and added FavoritesController.java and favorites-view.fxml

--- src/main/java/model/POI.java
+++ src/main/java/model/POI.java
@@ -4,13 +4,20 @@ public class POI {
 
     private String name;
     private String address;
+
+
     private String link;
 
     public POI(String link) {
         this.link = link;
+        //TODO: MAPS-API
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getLink() {
+        return link;
     }
 }
