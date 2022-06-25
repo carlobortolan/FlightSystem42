41bb5df8364f31e940a84bc06d746e99bfcf6b7b
Carlo Bortolan <carlobortolan@tun.de>
2022-06-25 20:03:54 +0200

    Gradle HelloControllerTest.java

--- src/test/java/hello/world/demo/HelloControllerTest.java
+++ src/test/java/hello/world/demo/HelloControllerTest.java
@@ -1,6 +1,14 @@
 package hello.world.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.RequestBuilder;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //@ContextConfiguration(classes = HelloController.class)
 class HelloControllerTest {
 
-//    @Autowired
+    @Autowired
 //    private MockMvc mockMvc;
 
     @Test
